17023173e0cb8e3c7066791726560888a890eea9
bearaxe <insetpun@gmail.com>
2017-08-10 22:30:54 -0400

    hacks to get s3 to stop spamming 0byte downloads

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { MainComponent } from './main/main.component';
 import { ErrorComponent } from './error/error.component';
 
 const appRoutes: Routes = [
-  { path: '', component: MainComponent },
+  { path: '', redirectTo: '/index.html', pathMatch: 'full' },
+  { path: 'index.html', component: MainComponent},
   { path: '**', component: ErrorComponent }
 ];
 
